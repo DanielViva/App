ac29444942c51b2163153eedeeb5c50e82046957
DanielViva <vivaddp@gmail.com>
2018-01-27 12:08:48 -0200

    Adicionei barcode

---
+++ app/src/main/java/com/example/vivad/app/CamActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vivad.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cam)
+    }
+}
